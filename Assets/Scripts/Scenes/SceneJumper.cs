18eca0c5c9f3f6ee15796be198aa720931f46b8d
Sampsoni <sami.repo@live.com>
2020-03-02 13:12:50 +0200

    Added scene for menu and game level Added ui-canvas for ui buttons Script for handling scene changes

---
+++ Assets/Scripts/Scenes/SceneJumper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneJumper : MonoBehaviour
+{
+    public void GoToGameScene()
+    {
+        //TODO - Constant in here instead of string. 
+        SceneManager.LoadScene("GameLevel", LoadSceneMode.Single);
+    }
+    public void GoToMenu()
+    {
+        //TODO - Constant in here instead of string. 
+        SceneManager.LoadScene("GameMenu", LoadSceneMode.Single);
+    }
+}
