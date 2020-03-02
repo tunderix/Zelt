ebe1bac66dc5baaee5d347e4e44fd2826401fd02
Sampsoni <sami.repo@live.com>
2020-03-02 19:12:42 +0200

    Added Constants File

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class Constants
+{
+    static class Scene
+    {
+        public const string Pi = 3.14159;
+    }
+}
